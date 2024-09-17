52de7515260a88aaacd9ce842fe54ee5f12a8e91
Sebastian <sebasrojasm1@gmail.com>
2024-09-17 15:29:24 -0500

    Is added classvalidators properties in the entity

--- src/module/user/entities/user.entity.ts
+++ src/module/user/entities/user.entity.ts
@@ -1,15 +1,23 @@
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString } from 'class-validator';
 
 @Schema({ timestamps: true })
-export class User extends Document{
-    @Prop({ required: true })
-    name: string;
+export class User extends Document {
+  @ApiProperty()
+  @Prop({ required: true })
+  @IsString()
+  name: string;
 
-    @Prop({ required: true, unique: true })
-    email: string;
+  @ApiProperty()
+  @Prop({ required: true, unique: true })
+  @IsEmail()
+  email: string;
 
-    @Prop({ required: true })
-    password: string;
+  @ApiProperty()
+  @Prop({ required: true })
+  @IsString()
+  password: string;
 }
 
 export const userSchema = SchemaFactory.createForClass(User);
