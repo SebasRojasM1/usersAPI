dde0e5c43410d9dd86b62e7f223e04885e5e62d6
Sebastian <sebasrojasm1@gmail.com>
2024-09-17 15:40:28 -0500

    The user dto was created succesfully

--- src/module/user/dto/create-user.dto.ts
+++ src/module/user/dto/create-user.dto.ts
@@ -1,1 +1,32 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8, {
+    message: 'The characters minimum is 8. Please complete your password',
+  })
+  @MaxLength(25, {
+    message:
+      'The maximum characters allowed are 25. Please restructure the password',
+  })
+  password: string;
+}
--- src/module/user/dto/update-user.dto.ts
+++ src/module/user/dto/update-user.dto.ts
@@ -1,4 +1,23 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @ApiProperty()
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty()
+  @IsString()
+  @MinLength(8, {
+    message: 'The characters minimum is 8. Please complete your password',
+  })
+  @MaxLength(25, {
+    message:
+      'The maximum characters allowed are 25. Please restructure the password',
+  })
+  password: string;
+}
