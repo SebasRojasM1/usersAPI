d00f5597ea37a1affc0167710c3677b5191263e7
Sebastian <sebasrojasm1@gmail.com>
2024-09-17 16:59:20 -0500

    The auth module was implemented the JWT register with the config

--- src/libs/auth/auth.module.ts
+++ src/libs/auth/auth.module.ts
@@ -1,8 +1,20 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { AuthService } from './services/auth.service';
 import { AuthController } from './controllers/auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { UtilsModule } from '../utils/utils.module';
+import { UserModule } from 'src/module/user/user.module';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.ACCESS_TOKEN_EXPIRY || '15m'  },
+    }),
+    UtilsModule,
+    UserModule
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
