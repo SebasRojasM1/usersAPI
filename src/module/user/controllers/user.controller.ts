f606400646bebe6ff5f411a82ec0390299902d4c
Sebastian <sebasrojasm1@gmail.com>
2024-09-17 16:00:01 -0500

    The user controller was created succesfully

--- src/module/user/controllers/user.controller.ts
+++ src/module/user/controllers/user.controller.ts
@@ -1,34 +1,55 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { UserService } from '../services/user.service';
+import { CreateUserDto, UpdateUserDto } from '../dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Users') 
 @Controller('user')
-export class UserController {
+export class UsersController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  @Post("create")
+  @ApiOperation({ summary: 'Create a user to the system.', description: 'Create a user to access the system.' })
+  @ApiResponse({status: 201, description: 'User created successfully.'})
+  @ApiResponse({status: 400, description: 'The data entered to create the user is invalid.'})
+  @ApiResponse({status: 500, description: 'An internal server error occurred while creating the user.'})
+  create(@Body() createUser: CreateUserDto) {
+    return this.userService.create(createUser);
   }
 
-  @Get()
+  @Get("/all")
+  @ApiOperation({ summary: 'Find all the users of the system.', description: 'View all users registered in the system.' })
+  @ApiResponse({status: 200, description: 'All users were found successfully.'})
+  @ApiResponse({status: 404, description: 'No users were found in the system.'})
+  @ApiResponse({status: 500,description: 'An internal server error occurred while searching for the users.'})
   findAll() {
     return this.userService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Find the user by ID of the system.', description: 'View a specific user registered in the database.' })
+  @ApiResponse({status: 200, description: 'User found successfully.',})
+  @ApiResponse({status: 404, description: 'User with the entered ID not found.'})
+  @ApiResponse({status: 500, description: 'An internal server error occurred while searching for the user.'})
   findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+    return this.userService.findOneById(id);
   }
 
-  @Patch(':id')
+  @Patch('update/:id')
+  @ApiOperation({ summary: 'Update a user to the system.', description: 'Update a specific user registered in the database.' })
+  @ApiResponse({status: 200, description: 'User updated successfully.'})
+  @ApiResponse({status: 404, description: 'User with the entered ID not found.'})
+  @ApiResponse({status: 500, description: 'An internal server error occurred while updating the user.'})
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
+    return this.userService.updateUser(id, updateUserDto);
   }
 
-  @Delete(':id')
+  @Delete('delete/:id')
+  @ApiOperation({ summary: 'Delete a user to the system.', description: 'Delete a user of the system.' })
+  @ApiResponse({status: 200, description: 'User deleted successfully.'})
+  @ApiResponse({status: 404, description: 'User with the entered ID not found.'})
+  @ApiResponse({status: 500, description: 'An internal server error occurred while deleting the user.'})
   remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+    return this.userService.deleteUser(id);
   }
 }
